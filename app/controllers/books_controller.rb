6058ed167c91e2e94cfe2d25348a56729cf7b991
EC2 Default User <ec2-user@ip-172-31-80-84.ec2.internal>
2022-09-12 05:52:45 +0000

    edit機能の実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,11 +6,12 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to "/books/show"
+    redirect_to book_path(book.id)
   end
    
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -18,6 +19,13 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
   
   private
